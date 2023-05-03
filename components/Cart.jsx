32a9e2c3b030d6a8f102fcc308047140166ba518
Your Name <you@example.com>
2023-05-03 19:55:10 +0200

    1

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -84,7 +84,7 @@ const Cart = () => {
           <div className='cart-bottom'>
             <div className='total'>
               <h3>Subtotal: </h3>
-              <h3>${totalPrice}</h3>
+              <h3>{totalPrice}zł</h3>
             </div>
             <div className='btn-container'>
                 <button 
